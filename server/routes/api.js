3b72006b592d60016363180df9b39399cf0df021
taylorhardy <446341@directv.com>
2017-12-13 11:09:07 -0600

    first commit

---
+++ server/routes/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const sendError = (err, res) => {
+	response.status = 501;
+	response.message = typeof err == 'object' ? err.message : err;
+	res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+	status: 200,
+	data: [],
+	message: null
+};
+
+module.exports = router;
